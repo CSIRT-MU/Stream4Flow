9dd8cc1696b525c3f53f5b72473e4b2d48411999
tomaspvk <433592@mail.muni.cz>
2017-07-14 12:02:13 +0200

    template for application creation added, along with module used in the template, that simplifies the application itself

---
+++ applications/application_template/application_template.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+
+#
+# MIT License
+#
+# Copyright (c) 2016 Tomas Pavuk <433592@mail.muni.cz>, Institute of Computer Science, Masaryk University
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+
+import argparse  # Arguments parser
+
+from modules import kafkaIO
+
+
+def process_results(data_to_process, producer, output_topic):
+    # Here you can format your results output and send it to the kafka topic
+    results_output = data_to_process
+
+    kafkaIO.send_data_to_kafka(results_output, producer, output_topic)
+
+
+def process_input(input_data):
+    # Here you can process input stream with MapReduce operations
+    modified_input = input_data
+
+    return modified_input
+
+
+if __name__ == "__main__":
+    # Define application arguments (automatically creates -h argument)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-iz", "--input_zookeeper", help="input zookeeper hostname:port", type=str, required=True)
+    parser.add_argument("-it", "--input_topic", help="input kafka topic", type=str, required=True)
+    parser.add_argument("-oz", "--output_zookeeper", help="output zookeeper hostname:port", type=str, required=True)
+    parser.add_argument("-ot", "--output_topic", help="output kafka topic", type=str, required=True)
+
+    # You can add your own arguments here
+    # See more at:
+    # https://docs.python.org/2.7/library/argparse.html
+
+    # Parse arguments
+    args = parser.parse_args()
+
+    # Initialize input stream and parse it into JSON
+    ssc, parsed_input_stream = kafkaIO.initialize_and_parse_input_stream(args.input_zookeeper, args.input_topic, 1)
+
+    # Process input in the desired way
+    processed_input = process_input(parsed_input_stream)
+
+    # Initialize kafka producer
+    kafka_producer = kafkaIO.initialize_kafka_producer(args.output_zookeeper)
+
+    # Process computed data and send them to the output
+    kafkaIO.process_data_and_send_result(processed_input, kafka_producer, args.output_topic, process_results)
+
+    # Start Spark streaming context
+    kafkaIO.spark_start(ssc)
