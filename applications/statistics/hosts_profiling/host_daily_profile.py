8349e720ca705f78aed9e1dce5c897764c6865d4
stefanik12 <stefanik.mich@gmail.com>
2017-07-19 16:27:02 +0200

    Implemented tested sample of host_daily_profile app to gather temporal statistics over all day window

--- applications/statistics/hosts_profiling/host_daily_profile.py
+++ applications/statistics/hosts_profiling/host_daily_profile.py
@@ -32,8 +32,9 @@ for each host each window contain:
     - a list of top n most active ports as sorted by a number of flows on a given port
 
 Usage:
+TODO
   top_n_host_stats.py -iz <input-zookeeper-hostname>:<input-zookeeper-port> -it <input-topic> -oh
-    <output-hostname>:<output-port> -n <max. # of ports for host> -net <CIDR network range>
+    <output-hostname>:<output-port> -n <max. # of ports for host>
 
   To run this on the Stream4Flow, you need to receive flows by IPFIXCol and make them available via Kafka topic. Then
   you can run the example
@@ -58,8 +59,106 @@ from pyspark.streaming.kafka import KafkaUtils  # Spark streaming Kafka receiver
 
 from collections import namedtuple
 
+# TODO: check these initial requirements in the end:
+
+# TODO: in the selected big window (supposedly 24h) is suggested to compute
+# * average number of communication partners for given hosts
+# * distinct ports for host, sorted by the frequency of activity
+# * frequency of communication times for host
+# * probability of the host being the server/client machine/network infrastructure based on
+# the distance of the selected attributes' values to mean values of the selected categories
+# stats_json.pprint(200)
+
+
+# casting structures
 IPStats = namedtuple('IPStats', 'ports dst_ips http_hosts')
-StatsItem = namedtuple('StatsItem', 'key flows type')
+StatsItem = namedtuple('StatsItem', 'packets bytes flows')
+
+ZERO_ITEM = StatsItem(0, 0, 0)  # neutral item used if no new data about the IP was collected in recent interval
+
+# temporal constants
+HOURLY_INTERVAL = 10  # aggregation interval for one item of temporal output array
+DAILY_INTERVAL = 40   # collection interval of aggregations as items in output array
+
+TIME_DIMENSION = DAILY_INTERVAL / HOURLY_INTERVAL
+print("Time dimension: %s" % TIME_DIMENSION)
+
+EMPTY_TIME_DIMENSIONS = [0] * TIME_DIMENSION
+
+INCREMENT = 0
+
+# temporal methods reassuring the temporal array consistency
+
+
+def increment():
+    """
+    increments the global counter that should keep consistent with the duration of the app run in hours
+    """
+    global INCREMENT
+    INCREMENT += 1
+
+
+def modulate_position(timestamp):
+    """
+    counts the position in time-sorted log of IP activity as based on the timestamp attached to
+    the particular log in rdd
+    timestamp: attached timestamp
+    """
+    result = (INCREMENT - timestamp) % TIME_DIMENSION
+    return result
+
+
+def update_array(array, position, value):
+    """
+    updates an array inserting a _value_ to a chosen _position_ in an _array_
+    overcomes a general restriction disabling to use an assignment in lambda statements
+    :param array: _array_
+    :param position: _position_
+    :param value: _value_
+    """
+    array[int(position)] = value
+    return array
+
+
+def initialize_array(value, timestamp):
+    """
+    initializes an empty array of default log length (=TIME_DIMENSION) with a _value_
+    inserted on a position at a given _timestamp_
+    :param value: _value_
+    :param timestamp: _timestamp_
+    """
+    return update_array(list([ZERO_ITEM] * TIME_DIMENSION), modulate_position(timestamp), value)
+
+
+def align_to_long_window(rdd_set):
+    """
+    transforms all RDDs from _rdd_set_ of a format (data, timestamp) into an initial log
+    of the size of the default log length (=TIME_DIMENSION)
+    the log contains TIME_DIMENSION of zeros except on a modulated timestamp position
+    filling zeros as missing values
+    :param rdd_set: _rdd_set_
+    """
+    return rdd_set.map(lambda rdd: (rdd[0], initialize_array(rdd[1][0], rdd[1][1])) \
+        if len(rdd[1]) == 2
+    else (rdd[0], rdd[1]))
+
+
+def merge_init_arrays(a1, a2):
+    """ Merges the given arrays so that the output array contains either value of a1, or a2 for each nonzero value
+    Arrays should be in disjunction append -1 when both arrays are filled, so the error is traceable
+    :param a1 array of the size of a2
+    :param a2 array of the size of a1
+    :return Merges arrays
+    """
+    merge = []
+    for i in range(len(a1)):
+        if a1[i] != ZERO_ITEM and a2[i] != ZERO_ITEM:
+            # should not happen
+            merge.append(-1)
+        else:
+            merge.append(a1[i] if a1[i] != ZERO_ITEM else a2[i])
+
+    return merge
 
 
 def send_data(data, output_host):
@@ -174,30 +273,62 @@ def process_results(json_rdd, n=10):
         send_data(json.dumps(result_dict)+"\n", args.output_host)
 
 
-def count_host_stats(flow_json):
+def collect_hourly_stats(stats_json):
     """
-    TODO
-
-    :type flow_json: Initialized spark streaming context, windowed, json_loaded.
+    Performs a hourly aggregation on input data, whose result is to be collected in items of daily aggregation
+    :type stats_json: Initialized spark streaming context, with data in json format as in host_stats application
     """
-    # TODO: placeholder for host profiling app based on windowed info from host_statistics applications
-    # TODO: in the selected big window (supposedly 24h) is suggested to compute
-    # * average number of communication partners for given hosts
-    # * distinct ports for host, sorted by the frequency of activity
-    # * frequency of communication times for host
-    # * probability of the host being the server/client machine/network infrastructure based on
-    # the distance of the selected attributes' values to mean values of the selected categories
-    # flow_json.pprint(200)
 
-    # Filter flows with required data, in a given address range
-    flow_with_keys = flow_json.filter(lambda json_rdd: ("ipfix.sourceIPv4Address" in json_rdd.keys()) and
-                                                       ("ipfix.destinationTransportPort" in json_rdd.keys()) and
-                                                       (ipaddress.ip_address(json_rdd["ipfix.sourceIPv4Address"]) in network_filter)
-                                      )
-    # Set window and slide duration for flows analysis
-    flow_with_keys_windowed = flow_with_keys.window(window_duration, window_slide)
+    stats_windowed = stats_json.window(HOURLY_INTERVAL, HOURLY_INTERVAL)
 
-    return None
+    stats_windowed_keyed = stats_windowed.map(lambda json_rdd: (json_rdd["src_ipv4"],
+                                                                (json_rdd["stats"]["total"]["packets"],
+                                                                 json_rdd["stats"]["total"]["bytes"],
+                                                                 json_rdd["stats"]["total"]["flow"])
+                                                                ))
+    ip_stats_sumed = stats_windowed_keyed.reduceByKey(lambda current, update: (current[0] + update[0],
+                                                                            current[1] + update[1],
+                                                                            current[2] + update[2]))
+
+    ip_stats_objected = ip_stats_sumed.mapValues(lambda avg_vals: (StatsItem(*avg_vals), INCREMENT))
+
+    return ip_stats_objected
+
+
+def collect_daily_stats(hourly_stats):
+    """
+    aggregation of the time stats of _small_window_data_ in a tuple format (data, timestamp) into a default log vector
+    in format [0, 0, ... , 0, 0, 0] containing the newest data at the beginning
+    appends a result of the new small window at the beginning of the time log for each IP address
+    :param hourly_stats: _hourly_stats_
+    """
+    global INCREMENT
+
+    # set a window of DAY_WINDOW_INTERVAL upon small window RDDs
+    long_window_base = hourly_stats.window(DAILY_INTERVAL, HOURLY_INTERVAL)
+
+    # Debug print - see how recent incoming RDDs are transformed after each HOUR_WINDOW_INTERVAL
+    # long_window_debug = long_window_base.map(lambda rdd: {"ip": rdd[0],
+    #                                                           "rdd_timestamp": rdd[1][1],
+    #                                                           "current_inc": INCREMENT,
+    #                                                           "mod_pos": modulate_position(int(rdd[1][1])),
+    #                                                           "value": rdd[1][0]})
+    # long_window_debug.pprint(17)
+
+    # log for each key(IP) is reduced from sparse logs of aggregated volume of activity for each time unit (=hour)
+    # first logs of small window in format IP: (volume, timestamp) are mapped into sparse vector=[0, 0, .. , volume, 0]
+    # where vector has a size of TIME_DIMENSION and volume inserted on modulated position (see modulate_position())
+    # then sparse vectors are combined by merge - "summing-up": nonzero positions (see merge_init_arrays())
+    long_window_data_stream = long_window_base.map(lambda rdd: (rdd[0], initialize_array(rdd[1][0], rdd[1][1]))) \
+        .reduceByKey(lambda current, update: merge_init_arrays(current, update))
+
+    # current position counter update should keep consistent with small window counter - increment on each new data
+    long_window_data_stream.reduce(lambda current, update: 1).foreachRDD(lambda rdd: increment())
+
+    long_window_data_stream.pprint(100)
+
+    # gives the current log after every new batch from small window
+    return long_window_data_stream.window(HOURLY_INTERVAL, DAILY_INTERVAL)
 
 
 if __name__ == "__main__":
@@ -205,9 +336,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-iz", "--input_zookeeper", help="input zookeeper hostname:port", type=str, required=True)
     parser.add_argument("-it", "--input_topic", help="input kafka topic", type=str, required=True)
-    parser.add_argument("-oh", "--output_host", help="output hostname:port", type=str, required=True)
-    parser.add_argument("-net", "--network_range", help="network range to watch", type=str, required=True)
-    parser.add_argument("-n", "--top_n", help="max. number of ports for a host to retrieve", type=int, required=False)
+
+    parser.add_argument("-oz", "--output_zookeeper", help="output zookeeper hostname:port", type=str, required=True)
+    parser.add_argument("-ot", "--output_topic", help="output kafka topic", type=str, required=True)
 
     # Parse arguments.
     args = parser.parse_args()
@@ -217,8 +348,6 @@ if __name__ == "__main__":
     kafka_partitions = 1  # Number of partitions of the input Kafka topic
     window_duration = 10  # Analysis window duration (10 seconds)
     window_slide = 10  # Slide interval of the analysis window (10 seconds)
-    # Filter for network for detection (regex filtering), e.g. "10\.10\..+"
-    network_filter = ipaddress.ip_network(unicode(args.network_range, "utf8"))
 
     # Spark context initialization
     sc = SparkContext(appName=application_name + " " + " ".join(sys.argv[1:]))  # Application name used as the appName
@@ -232,10 +361,12 @@ if __name__ == "__main__":
     input_stream_json = input_stream.map(lambda x: json.loads(x[1]))
 
     # Process data to the defined function.
-    host_statistics = count_host_stats(input_stream_json)
+    hourly_host_statistics = collect_hourly_stats(input_stream_json)
+    daily_host_statistics = collect_daily_stats(hourly_host_statistics)
 
     # Transform computed statistics into desired json format and send it to output_host as given in -oh input param
-    stats_json = host_statistics.foreachRDD(lambda rdd: process_results(rdd.collectAsMap()))
+    # hourly_host_statistics.foreachRDD(lambda rdd: rdd.pprint())
+    # daily_host_statistics.pprint(100)
 
     # Start input data processing
     ssc.start()
