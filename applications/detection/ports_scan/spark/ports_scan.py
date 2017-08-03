3cf8dff89b68cc7899d3948a4b1fb7571e2aafa0
tomaspvk <433592@mail.muni.cz>
2017-08-03 10:43:07 +0200

    Modified detection script to work with latest version of framework

--- applications/detection/ports_scan/spark/ports_scan.py
+++ applications/detection/ports_scan/spark/ports_scan.py
@@ -25,59 +25,31 @@
 #
 
 """
-Detects horizontal and vertical TCP ports scans on the network using adjustable threshold for number of flows
+Detects horizontal and vertical port scans on the network using adjustable threshold for number of flows
 
 Default values are:
     * window size: 60
     * min amount of flows: 20
 
 Usage:
-    ports_scan.py -iz <input-zookeeper-hostname>:<input-zookeeper-port> -it <input-topic>
-    -oh <output-hostname>:<output-port>
+    portscan.py -iz <input-zookeeper-hostname>:<input-zookeeper-port> -it <input-topic>
+    -oz <output-zookeeper-hostname>:<output-zookeeper-port> -ot <output-topic>
 
 To run this on the Stream4Flow, you need to receive flows by IPFIXCol and make them available via Kafka topic. Then you
 can run the application
-    $ ./run-application.sh ./detection/ports_scan/spark/ports_scan.py -iz producer:2181\
-    -it ipfix.entry -oh consumer:20101
+    $ ./run-application.sh ./detection/ssh_auth_simple/spark/portscan.py -iz producer:2181\
+    -it ipfix.entry -oz producer:9092 -ot results.output
 """
 
 
 import sys  # Common system functions
 import os  # Common operating system functions
 import argparse  # Arguments parser
-import ujson as json  # Fast JSON parser
-import socket  # Socket interface
+
 import time  # Unix time to timestamp conversion
 
 from termcolor import cprint  # Colors in the console output
-
-from pyspark import SparkContext  # Spark API
-from pyspark.streaming import StreamingContext  # Spark streaming API
-from pyspark.streaming.kafka import KafkaUtils  # Spark streaming Kafka receiver
-
-
-def send_data(data, output_host):
-    """
-    Send given data to the specified host using standard socket interface.
-
-    :param data: data to send
-    :param output_host: data receiver in the "hostname:port" format
-    """
-
-    # Split outputHost hostname and port
-    host = output_host.split(':')
-
-    # Prepare a TCP socket.
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    # Connect to the outputHost and send given data
-    try:
-        sock.connect((host[0], int(host[1])))
-        sock.send(data)
-    except socket.error:
-        cprint("[warning] Unable to connect to host " + output_host, "blue")
-    finally:
-        sock.close()
+from modules import kafkaIO   # Module for kafka IO operations
 
 
 # Saves scans in dictionary, so 1 scan is not reported multiple times
@@ -120,28 +92,29 @@ def get_output_json(key, value, flows_increment):
     timestamp = '%s.%03d' % (time.strftime('%Y-%m-%dT%H:%M:%S', time.gmtime(s)), ms) + 'Z'
 
     if key[0] == 'horizontal':
-        output_json += "{\"@type\": \"ports_scan_" + key[0] + "\", \"src_ip\": \"" + key[1] + "\", \"dst_port\": \"" \
+        output_json += "{\"@type\": \"portscan_" + key[0] + "\", \"src_ip\": \"" + key[1] + "\", \"dst_port\": \"" \
                        + str(key[2]) + "\", \"dst_ips\": \"" + str(value[1]) + "\", \"flows\": " \
                        + str(value[0]) + ", " "\"duration_in_milliseconds\": " + str(value[2]) + \
                        ", \"timestamp\": \"" + str(timestamp) + \
                        "\", \"flows_increment\": " + str(flows_increment) + \
-                       ", \"targets_count\": " + str(value[4]) + "}\n"
+                       ", \"target_count\": " + str(value[4]) + "}\n"
     if key[0] == 'vertical':
-        output_json += "{\"@type\": \"ports_scan_" + key[0] + "\", \"src_ip\": \"" + key[1] + "\", \"dst_ip\": \"" \
+        output_json += "{\"@type\": \"portscan_" + key[0] + "\", \"src_ip\": \"" + key[1] + "\", \"dst_ip\": \"" \
                        + str(key[2]) + "\", \"dst_ports\": \"" + str(value[1]) + "\", \"flows\": " \
                        + str(value[0]) + ", " "\"duration_in_milliseconds\": " + str(value[2]) + \
                        ", \"timestamp\": \"" + str(timestamp) + \
                        "\", \"flows_increment\": " + str(flows_increment) + \
-                       ", \"targets_count\": " + str(value[4]) + "}\n"
+                       ", \"target_count\": " + str(value[4]) + "}\n"
     return output_json
 
 
-def process_results(results, output_host, s_window_duration):
+def process_results(results, producer, output_topic, s_window_duration):
     """
     Check if attack was reported, or additional flows were detected in same attack and report it
 
     :param results: flows that should be reported as attack
-    :param output_host: where to send processed data
+    :param producer: producer that sends the data
+    :param output_topic: name of the receiving kafka topic
     :param s_window_duration: window size in seconds
     """
 
@@ -165,17 +138,17 @@ def process_results(results, output_host, s_window_duration):
         # Check if dictionary cleaning is necessary
         clean_old_data_from_dictionary(s_window_duration)
 
-        # Send results to the specified host
-        send_data(output_json, output_host)
+        # Send desired output to the output_topic
+        kafkaIO.send_data_to_kafka(output_json, producer, output_topic)
 
 
 def get_key_with_ip_version(record, wanted_key):
     """
-    Find ipv4 type of key if present, ipv6 otherwise
+        Find ipv4 type of key if present, ipv6 otherwise
 
-    :param record: JSON record searched for key
-    :param wanted_key: string from which key will be made and searched (e.g. "source" => ipfix.sourceIPv4Address)
-    :return: value corresponding to the key in the record
+        :param record: JSON record searched for key
+        :param wanted_key: string from which key will be made and searched (e.g. "source" => ipfix.sourceIPv4Address)
+        :return: value corresponding to the key in the record
     """
 
     key_name = "ipfix." + wanted_key + "IPv4Address"
@@ -185,7 +158,7 @@ def get_key_with_ip_version(record, wanted_key):
     return record[key_name]
 
 
-def check_for_attacks_ssh(flows_stream, flows_threshold, s_window_duration, s_window_slide):
+def process_input(flows_stream, flows_threshold, s_window_duration, s_window_slide):
 
     # Check required flow keys
     flows_stream_with_keys = flows_stream.filter(lambda flow_json: ("ipfix.tcpControlBits" in flow_json.keys())
@@ -245,7 +218,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-iz", "--input_zookeeper", help="input zookeeper hostname:port", type=str, required=True)
     parser.add_argument("-it", "--input_topic", help="input kafka topic", type=str, required=True)
-    parser.add_argument("-oh", "--output_host", help="output hostname:port", type=str, required=True)
+    parser.add_argument("-oz", "--output_zookeeper", help="output zookeeper hostname:port", type=str, required=True)
+    parser.add_argument("-ot", "--output_topic", help="output kafka topic", type=str, required=True)
     parser.add_argument("-w", "--window_size", help="window size (in seconds)", type=int, required=False, default=60)
 
     # arguments for detection
@@ -260,23 +234,20 @@ if __name__ == "__main__":
     kafka_partitions = 1  # Number of partitions of the input Kafka topic
     window_duration = args.window_size  # Analysis window duration (60 seconds default)
     window_slide = 1  # Slide interval of the analysis window (1 second)
+    microbatch_duration = 1  # Microbatch duration is 1 second
 
-    # Spark context initialization
-    sc = SparkContext(appName=application_name + " " + " ".join(sys.argv[1:]))  # Application name used as the appName
-    ssc = StreamingContext(sc, 1)  # Spark microbatch is 1 second
+    # Initialize input stream and parse it into JSON
+    ssc, parsed_input_stream = kafkaIO.initialize_and_parse_input_stream(args.input_zookeeper, args.input_topic,
+                                                                         microbatch_duration)
 
-    # Initialize input DStream of flows from specified Zookeeper server and Kafka topic
-    input_stream = KafkaUtils.createStream(ssc, args.input_zookeeper, "spark-consumer-" + application_name,
-                                           {args.input_topic: kafka_partitions})
+    # Check for port scans
+    processed_input = process_input(parsed_input_stream, args.flows_threshold, window_duration, window_slide)
 
-    # Parse flows in the JSON format
-    flows_json = input_stream.map(lambda line: json.loads(line[1]))
+    # Initialize kafka producer
+    kafka_producer = kafkaIO.initialize_kafka_producer(args.output_zookeeper)
 
-    # Check for portscans
-    attacks = check_for_attacks_ssh(flows_json, args.flows_threshold, window_duration, window_slide)
+    # Process computed data and send them to the output
+    kafkaIO.process_data_and_send_result(processed_input, kafka_producer, args.output_topic, window_duration, process_results)
 
-    # Process computed statistics and send them to the standard output
-    attacks.foreachRDD(lambda rdd: process_results(rdd.collectAsMap(), args.output_host, window_duration))
-
-    ssc.start()
-    ssc.awaitTermination()
+    # Start Spark streaming context
+    kafkaIO.spark_start(ssc)
