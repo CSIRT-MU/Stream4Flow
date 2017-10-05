715ce710e0da7ad5f3ec9059390b139ce5d80124
Milan Cermak <cermak@ics.muni.cz>
2017-10-05 17:04:01 +0200

    Use src_ip and dst_ip instead of src_ip.raw or dst_ip.raw

--- applications/detection/ssh_auth_simple/web-interface/controllers/ssh_auth_simple.py
+++ applications/detection/ssh_auth_simple/web-interface/controllers/ssh_auth_simple.py
@@ -60,8 +60,8 @@ def get_histogram_statistics():
         # Set filter
         if filter != 'none':
             elastic_should = []
-            elastic_should.append({'term': {'src_ip.raw': filter}})
-            elastic_should.append({'term': {'dst_ip.raw': filter}})
+            elastic_should.append({'term': {'src_ip': filter}})
+            elastic_should.append({'term': {'dst_ip': filter}})
             elastic_bool.append({'bool': {'should': elastic_should}})
         # Prepare query
         qx = Q({'bool': {'must': elastic_bool}})
@@ -69,7 +69,7 @@ def get_histogram_statistics():
         # Get histogram data
         search_histogram = Search(using=client, index='_all').query(qx)
         search_histogram.aggs.bucket('by_time', 'date_histogram', field='@timestamp', interval=aggregation) \
-            .bucket('by_src', 'terms', field='src_ip.raw', size=2147483647) \
+            .bucket('by_src', 'terms', field='src_ip', size=2147483647) \
             .bucket('sum_of_flows', 'sum', field='flows_increment')
         histogram = search_histogram.execute()
 
@@ -130,16 +130,16 @@ def get_top_n_statistics():
         # Set filter
         if filter != 'none':
             elastic_should = []
-            elastic_should.append({'term': {'src_ip.raw': filter}})
-            elastic_should.append({'term': {'dst_ip.raw': filter}})
+            elastic_should.append({'term': {'src_ip': filter}})
+            elastic_should.append({'term': {'dst_ip': filter}})
             elastic_bool.append({'bool': {'should': elastic_should}})
         # Prepare query
         qx = Q({'bool': {'must': elastic_bool}})
 
         # Get ordered data (with maximum size aggregation)
         search = Search(using=client, index='_all').query(qx)
-        search.aggs.bucket('by_src', 'terms', field='src_ip.raw', size=2147483647)\
-              .bucket('by_dst', 'terms', field='dst_ip.raw', size=2147483647)\
+        search.aggs.bucket('by_src', 'terms', field='src_ip', size=2147483647)\
+              .bucket('by_dst', 'terms', field='dst_ip', size=2147483647)\
               .bucket('top_src_dst', 'top_hits', size=1, sort=[{'@timestamp': {'order': 'desc'}}])
         results = search.execute()
 
@@ -200,15 +200,15 @@ def get_attacks_list():
         # Set filter
         if filter != 'none':
             elastic_should = []
-            elastic_should.append({'term': {'src_ip.raw': filter}})
-            elastic_should.append({'term': {'dst_ip.raw': filter}})
+            elastic_should.append({'term': {'src_ip': filter}})
+            elastic_should.append({'term': {'dst_ip': filter}})
             elastic_bool.append({'bool': {'should': elastic_should}})
         qx = Q({'bool': {'must': elastic_bool}})
 
         # Search with maximum size aggregations
         search = Search(using=client, index='_all').query(qx)
-        search.aggs.bucket('by_src', 'terms', field='src_ip.raw', size=2147483647) \
-              .bucket('by_dst', 'terms', field='dst_ip.raw', size=2147483647) \
+        search.aggs.bucket('by_src', 'terms', field='src_ip', size=2147483647) \
+              .bucket('by_dst', 'terms', field='dst_ip', size=2147483647) \
               .bucket('top_src_dst', 'top_hits', size=1, sort=[{'@timestamp': {'order': 'desc'}}])
         results = search.execute()
 
