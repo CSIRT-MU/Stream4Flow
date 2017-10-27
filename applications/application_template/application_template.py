acc000b4e91f3f2343a3f63ce1369614210c9c5b
Milan Cermak <cermak@ics.muni.cz>
2017-10-27 10:57:43 +0200

    Small documentation update

--- applications/application_template/application_template.py
+++ applications/application_template/application_template.py
@@ -35,7 +35,7 @@ Usage:
 
 To run this on the Stream4Flow, you need to receive flows by IPFIXCol and make them available via Kafka topic. Then
 you can run the application as follows:
-    $ ~/applications//run-application.sh ./application_template.py -iz producer:2181 -it ipfix.entry
+    $ ~/applications/run-application.sh ./application_template.py -iz producer:2181 -it ipfix.entry
     -oz producer:9092 -ot results.output
 """
 
