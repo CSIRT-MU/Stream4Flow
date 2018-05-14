bd31d76dbffb04b552f7c3073429f24672d5e0ab
Tomas Jirsik <jirsik@ics.muni.cz>
2018-05-14 13:52:47 +0200

    Updated help

--- applications/statistics/protocols_statistics/spark/protocols_statistics.py
+++ applications/statistics/protocols_statistics/spark/protocols_statistics.py
@@ -34,8 +34,8 @@ Usage:
 
 To run this on the Stream4Flow, you need to receive flows by IPFIXCol and make them available via Kafka topic. Then
 you can run the application as follows:
-    $ ~/applications/run-application.sh ./protocols_statistics.py -iz producer:2181 -it ipfix.entry
-    -oz producer:9092 -ot results.output
+    $ ~/applications//run-application.sh ./statistics/protocols_statistics/spark/protocols_statistics.py -iz producer:2181 
+    -it ipfix.entry -oz producer:9092 -ot results.output
 """
 
 
